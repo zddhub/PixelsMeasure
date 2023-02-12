12a7ac72819f82640ac94b9c7afd137f8a5fddc0
zdd <zddhub@gmail.com>
2023-02-12 23:41:16 +0800

    Set publicPath for github page

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/PixelsMeasure/" : "/",
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
