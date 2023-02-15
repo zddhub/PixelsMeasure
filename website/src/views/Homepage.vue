f33b59a613a3cf860c518933a2a65a1fbe4333fb
zdd <zddhub@gmail.com>
2023-02-16 00:40:57 +0800

    Add Landing content

--- website/src/views/Homepage.vue
+++ website/src/views/Homepage.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <hero></hero>
+        <landing></landing>
     </div>
 </template>
 <script>
 import Hero from "./components/Hero";
+import Landing from "./Landing.vue";
 
 
 export default {
   name: "homepage",
   components: {
-    Hero
+    Hero,
+    Landing
   }
 };
 </script>
