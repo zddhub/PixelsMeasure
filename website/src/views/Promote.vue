d1df0f7487f85e6e173900fd0b5623f09fc84f72
zdd <zddhub@gmail.com>
2023-09-21 00:21:11 +0800

    Add qr code

--- website/src/views/Promote.vue
+++ website/src/views/Promote.vue
@@ -3,17 +3,21 @@
         <section class="section">
             <div class="container">
                 <div class="border-0 ">
-                    <div class="p-5">
+                    <div class="p-4">
                         <div class="row align-items-center justify-content-center">
-                            <div class="col-lg-10 text-center">
+                            <div class="col-lg-6 promote">
                                 <h3 class="big-title">{{ promote.title }}</h3>
                                 <p class="mt-0 small-text">{{ promote.subtitle }}</p>
+                                <primary-button :text="promote.download.title" :url="promote.download.url"></primary-button>
+                            </div>
+
+
+                            <div class="col-lg-4 text-center">
+                                <img class="qr-code" src="img/qr-code-blue.png" />
+                                <p>Scan to download</p>
                             </div>
                         </div>
 
-                        <div class="row align-items-center justify-content-center">
-                            <primary-button :text="promote.download.title" :url="promote.download.url"></primary-button>
-                        </div>
                     </div>
                 </div>
             </div>
